378a7458d1c5263132768fd7164336fcecaabd9c
Alexandre Beauverger <alexandrebeauverger@gmail.com>
2017-12-01 13:22:00 +0100

    parse element of xml in python class

--- appli/back/data/osm-xml-parse.py
+++ appli/back/data/osm-xml-parse.py
@@ -1,11 +1,58 @@
 import os
+import json
 from lxml import etree
 
+class rue:
+	nom = "no name"
+	fonction = "no fonction"
+	bicycle = "no bicycle"
+	maxspeed = "no max speed"
+	oneway = "none"
+	points =[]
+
+i = 0
 tree = etree.parse("map.osm")
 
 for element in tree.xpath("way"):
 	way = element
 
 	for tag in way.xpath("tag"):
-		attrib = tag.attrib
-		print(attrib)
+		road = tag.attrib
+
+		if road["k"] == "highway":
+			test = rue()
+			i = i+1
+
+			for tag in way.xpath("tag"):
+				name = tag.attrib
+
+				if name["k"] == "name":
+					test.nom = name["v"]
+
+				if name["k"] == "highway":
+					test.fonction = road["v"]
+
+				if name["k"] == "bicycle":
+					test.bicycle = road["v"]
+
+				if name["k"] == "maxspeed":
+					test.maxspeed = road["v"]
+
+				if name["k"] == "oneway":
+					test.oneway = road["v"]
+
+			test.points = []
+			for nd in way.xpath("nd"):
+				nd_ref = nd.attrib["ref"]
+				for node in tree.xpath("node"):
+					if node.attrib["id"] == nd_ref:
+						test.points.append([node.attrib["lon"],node.attrib["lat"]])
+
+	print(test.nom)
+	print(test.fonction)
+	print(test.bicycle)
+	print(test.maxspeed)
+	print(test.oneway)
+	print(test.points)
+	print(json.dump(test, indent=4, io))
+print(i)
---
+++ appli/back/data/run.sh
@@ -0,0 +1,1 @@
+python3 osm-xml-parse.py
