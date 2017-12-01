d41af918903aefded277544f41ac8bd42a9e9669
salaris <alexandrebeauverger@gmail.com>
2017-12-01 08:01:43 +0100

    parse of the way element

--- appli/back/data/osm-xml-parse.py
+++ appli/back/data/osm-xml-parse.py
@@ -3,10 +3,9 @@ from lxml import etree
 
 tree = etree.parse("map.osm")
 
-brut = tree.findall('way')
+for element in tree.xpath("way"):
+	way = element
 
-for way in brut:
-	tag = way.findall('tag').text
-	print(tag)
-
-#je sais pas faire de commentaire en python donc tu l'enlèveras comme un grand, je t'aime fort et je suis sûre que tu vas t'en sortir avec ton script tout pourri parce que tu es le meilleur. Plein de love sur ton bon boule <3<3<3
+	for tag in way.xpath("tag"):
+		attrib = tag.attrib
+		print(attrib)
