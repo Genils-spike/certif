a55225ec4ff1487a0531490df96a198e43b99cef
salaris <alexandrebeauverger@gmail.com>
2017-12-24 16:55:07 +0100

    call of road works and insertion in database

--- appli/back/data/api_calls.py
+++ appli/back/data/api_calls.py
@@ -1,6 +1,44 @@
-import pymongo
-import json
-import sys
+import pymongo, json, requests, sys, time
 
 client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-db = sys.argv[1]
+db= client[sys.argv[1]]
+collection =  db[sys.argv[2]]
+
+if sys.argv[1] == "road_works":
+	if sys.argv[2] == "rennes_metropole":
+		api_url = "http://travaux.data.rennesmetropole.fr/api/roadworks"
+		api = requests.get(api_url).json()
+
+		class road_works :
+			works_id = 0
+			insert_date_hour = None
+			city = "undefined"
+			location = "undefined"
+			adress = "undefined"
+			begin_date = "undefined"
+			ending_date = "undifined"
+			works_type = "undefined"
+			works_name = "unedefined"
+			works_level = "undefined"
+			works_location = False
+
+		for events in api["features"]:
+			tmp = road_works()
+			properties = events["properties"]
+
+			#creation of the road_works database enteries		
+			
+			tmp.works_id = properties["id"]
+			tmp.insert_date_hour = time.localtime()
+			tmp.city = properties["commune"]
+			tmp.location = properties["quartier"]
+			tmp.adress = properties["localisation"]
+			tmp.begin_date = properties["date_deb"]
+			tmp.ending_date = properties['date_fin']
+			tmp.works_type = properties["type"]
+			tmp.works_name = properties["libelle"]
+			tmp.works_level = properties["niv_perturbation"]
+			tmp.works_location = events["geometry"]
+
+			collection.insert(tmp.__dict__)
+	
